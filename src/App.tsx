c0fd3237e260adf6f6dbf3a6cbd53288d222517b
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 18:49:55 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/70bc11b2-c78f-41a3-9d80-7cd38d33cd8d/src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CategoryPage from './pages/Category'
+import ProductPage from './pages/Product'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/category/:categoryId" element={<CategoryPage />} />
-      <Route path="/product/:productId" element={<div>Product Page</div>} />
+      <Route path="/product/:productId" element={<ProductPage />} />
       <Route path="/cart" element={<div>Cart Page</div>} />
       <Route path="/checkout" element={<div>Checkout Page</div>} />
     </Routes>
