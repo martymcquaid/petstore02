6b60f9b66c37a5bfd954a17ff4fcbc5222069b95
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 21:02:14 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/70bc11b2-c78f-41a3-9d80-7cd38d33cd8d/src/components/Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useCart } from '../context/CartContext'
 import { useWishlist } from '../context/WishlistContext'
 import { useTheme } from '../context/ThemeContext'
 import { useState, useEffect, useRef } from 'react'
 import { mockProducts } from '../data/products'
 import { Product } from '../types'
+import { scrollToTop } from '../utils/scrollToTop'
 
 export default function Header() {
   const { state } = useCart()
