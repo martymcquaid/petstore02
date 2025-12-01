2f32492bb16ba43b3d797df213e685755fe29726
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 20:30:45 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/70bc11b2-c78f-41a3-9d80-7cd38d33cd8d/src/components/ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from '../types'
 import { Link } from 'react-router-dom'
 import { useCart } from '../context/CartContext'
+import { useWishlist } from '../context/WishlistContext'
 
 interface ProductCardProps {
   product: Product
