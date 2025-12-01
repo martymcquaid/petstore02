f02dfa01ccda868cb3eb5ea04e5a2fa4af359f20
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 20:07:16 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/70bc11b2-c78f-41a3-9d80-7cd38d33cd8d/src/pages/OrderConfirmation.tsx

--- src/pages/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation.tsx
@@ -5,7 +5,7 @@ import Button from '../components/Button'
 
 export default function OrderConfirmationPage() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white dark:bg-gray-900">
       <Header />
 
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
