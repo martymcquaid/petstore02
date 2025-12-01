3856a612f32cf35acde515cd92abf865bfced8c2
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 20:02:10 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/70bc11b2-c78f-41a3-9d80-7cd38d33cd8d/src/pages/Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white dark:bg-gray-900">
       <Header />
 
       {/* Hero Section */}
